f9b22cef6708eac8d77e6bf1f11f32f0da2e90cc
AlexGyver <beragumbo@ya.ru>
2024-09-03 22:04:21 +0300

    add

---
+++ src/app.js
@@ -0,0 +1,75 @@
+import { Component } from '@alexgyver/component';
+
+const projects_list = 'https://raw.githubusercontent.com/AlexGyver/ota-projects/main/projects.txt';
+
+export default class App {
+    constructor() {
+        this.load();
+    }
+
+    async load() {
+        Component.make('div', {
+            parent: document.body,
+            context: this,
+            class: 'main',
+            children: [
+                {
+                    tag: 'div',
+                    class: 'header',
+                    text: 'AlexGyver OTA',
+                },
+                {
+                    tag: 'div',
+                    class: 'projects',
+                    var: 'projects',
+                }
+            ]
+        });
+
+        let projects = await fetch(projects_list, { cache: "no-store" });
+        projects = await projects.text();
+        projects = projects.split(/\r?\n/);
+
+        for (let proj of projects) {
+            Component.make('div', {
+                context: this,
+                parent: this.$projects,
+                var: proj,
+                class: 'project',
+                children: [
+                    {
+                        tag: 'span',
+                        class: 'project_label',
+                        text: proj,
+                        events: {
+                            click: () => window.open('https://github.com/AlexGyver/' + proj),
+                        },
+                    },
+                    {
+                        tag: 'div',
+                        class: 'icon down',
+                        events: {
+                            click: () => {
+                                this['$' + proj + '_btn'].click();
+                            }
+                        },
+                    }
+                ]
+            });
+
+            Component.make('esp-web-install-button', {
+                context: this,
+                parent: document.body,
+                style: 'display:none',
+                var: proj + '_btn',
+                attrs: {
+                    manifest: 'https://raw.githubusercontent.com/AlexGyver/' + proj + '/main/project.json',
+                },
+                child: {
+                    tag: 'button',
+                    slot: 'activate',
+                }
+            });
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css'
+import App from './app';
+
+document.addEventListener("DOMContentLoaded", async () => {
+    new App();
+});
